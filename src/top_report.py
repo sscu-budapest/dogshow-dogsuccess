556fed0e93f6607508d52ff922a8d6b143b4d970
Endre Márk Borza <endremborza@gmail.com>
2022-04-07 02:02:17 +0200

    build

--- src/top_report.py
+++ src/top_report.py
@@ -7,7 +7,6 @@ top_tables_report = dz.ReportFile("tops.md")
 deps = [sex_match_table, size_count_table]
 
 
-@dz.register(dependencies=deps, outputs_nocache=[top_tables_report])  # remove in v1.0
 def calculate_sex_match(show_top):
 
     sm_df = sex_match_table.get_full_df().sort_values(
